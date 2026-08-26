9cf98da7dcac7d6d9adde9118a91a720166fa316
ntcapslmao <muhammad.ghazi51@ui.ac.id>
2026-08-26 15:25:40 +0700

    Correcting past mistakes (secret key hidden)

--- tutorial0/settings.py
+++ tutorial0/settings.py
@@ -29,13 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9dk2dc@zusp!!gu-0dg@u(17rz7w6uco6*2+*b7x3t1m!u*t08'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = not PRODUCTION
 
 # Application definition
 
